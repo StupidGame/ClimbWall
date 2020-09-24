2fd9898c6529c97ac1861995c859ff33623281d8
matsuo877 <matsuo877@gmail.com>
2020-09-24 19:52:53 +0900

    初コミット

---
+++ src/matsuo/ClimbWall/main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace matsuo\mabodoufu;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\Server;
+
+
+class Main extends PluginBase implements Listener{
+
+
+  public function onEnable() {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  } 
+
+  public function onMove(PlayerMoveEvent $event){
+     
+   $player = $event->getPlayer();
+     if ($player->isSneaking()) {
+       if($player->getTargetBlock(1)->getId()!== 0){
+       $player->setCanClimbWalls(true); 
+        }else{
+       $player->setCanClimbWalls(false);
+        }
+        }else{ 
+          $player->setCanClimbWalls(false);
+        }
+  }  
+}
+

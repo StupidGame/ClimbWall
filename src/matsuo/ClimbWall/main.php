afaf967f50d0a5433d4c3bfcbe1883e72ca777c3
matsuo877 <matsuo877@gmail.com>
2020-10-01 19:53:29 +0900

    インデントを調整

--- src/matsuo/ClimbWall/main.php
+++ src/matsuo/ClimbWall/main.php
@@ -12,20 +12,33 @@ class Main extends PluginBase implements Listener{
 
 
   public function onEnable() {
+
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    
   } 
 
   public function onMove(PlayerMoveEvent $event){
      
    $player = $event->getPlayer();
-     if ($player->isSneaking()) {
+
+   if ($player->isSneaking()) {
+
        if($player->getTargetBlock(1)->getId()!== 0){
+
        $player->setCanClimbWalls(true); 
+
         }else{
+
        $player->setCanClimbWalls(false);
+
         }
+
         }else{ 
+
           $player->setCanClimbWalls(false);
+
       }
-  }  
+
+  } 
+
 }
